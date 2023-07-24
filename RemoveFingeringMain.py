fea97981a4461d40821d77fa2c29e7ea4ce240c6
ZhengjcVP <139152625+ZhengjcVP@users.noreply.github.com>
2023-07-24 11:16:11 +0800

    Updated to v1.4 alpha
    Improved performance on window scaling and refresh. See detail in changelog.

--- RemoveFingeringMain.py
+++ RemoveFingeringMain.py
@@ -7,15 +7,18 @@ import os
 import cv2 as cv
 from PIL import Image as ImagePIL
 
+import ctypes
+from win32api import GetSystemMetrics
+
 start_time = time.time()
 
 base_path="C:\\Users\\Zhengjc\\Pictures\\Saved Pictures\\"
-file_path=base_path+"Chopin Score pages\\2. Chopin Etudes Op.10 Henle\\Despeckled"
+file_path=base_path+"Other Composer\\Schubert Sonata Wiener\\Schubert D784 Test"
 
-file_name="Chopin Etudes Op 10 Henle"
+file_name="Schubert D784 Test"
 
-start_page=24
-page_num=26
+start_page=16
+page_num=19
 
 print("Folder Name: "+file_name)
 print("\tProcessing from Page",str(start_page),"to",str(page_num-1))
@@ -37,13 +40,22 @@ os.mkdir(final_path)
 # Pre-exist folder for matching templet. Currently not used.
 template_path=str(current_loc)+file_name+"_RF_Number\\"
 
+ctypes.windll.user32.SetProcessDPIAware()
+
 ix=-1
 iy=-1
 Dragging=False
+Scaling=0.3
+ScreenW = GetSystemMetrics(0)
+ScreenH = GetSystemMetrics(1)
+Window_Name="Press Enter for Next Image"
+# Proportion of window on screen. Must be <=1. 
+# 1 is not recomended because the taskbar will cover part of image
+Windows_Size=0.9
 
 def readMouseInput(event, x, y, flags, param):
       
-    global ix, iy, Dragging, img, prev_img, prev_img_2
+    global ix, iy, Dragging, img, prev_img, prev_img_2, Scaling
     
     RButton=False
 
@@ -54,16 +66,18 @@ def readMouseInput(event, x, y, flags, param):
                 prev_img_2=prev_img.copy()
                 prev_img=img.copy()
         Dragging = True
-        ix = x
-        iy = y
+        ix = int(x/Scaling)
+        iy = int(y/Scaling)
+        showImageAndResize(img)
 
     elif event == cv.EVENT_MOUSEMOVE:
         if Dragging == True:
-            ix = x
-            iy = y
+            ix = int(x/Scaling)
+            iy = int(y/Scaling)
             if(img[iy, ix]==0):
-                img=EraseNonConnected(img, ix, iy, area_thresh=350, brushsize=16)
+                img=EraseNonConnected(img, ix, iy, area_thresh=500, brushsize=24)
                 #time.sleep(0.05)
+                showImageAndResize(img)
 
     # Right Mouse Button undo the last step
     elif event == cv.EVENT_RBUTTONDOWN:
@@ -73,18 +87,33 @@ def readMouseInput(event, x, y, flags, param):
             img=prev_img.copy()
             prev_img=prev_img_2.copy()
             RButton=False
+            showImageAndResize(img)
 
     elif event == cv.EVENT_LBUTTONUP:
         Dragging = False
 
     # Middle Button erases fingerings within line
     elif event == cv.EVENT_MBUTTONDOWN:
-        ix = x
-        iy = y
+        ix = int(x/Scaling)
+        iy = int(y/Scaling)
         prev_img_2=prev_img.copy()
         prev_img=img.copy()
-        img=EraseConnected(img, ix, iy, brushsize=16)
+        img=EraseConnected(img, ix, iy, brushsize=24)
+        showImageAndResize(img)
         #time.sleep(0.05)
+
+def showImageAndResize(img, isInMain=False):
+    global Scaling
+    H, W = img.shape[:2]
+    Scaling=min(ScreenW/W, ScreenH/H)*Windows_Size
+    img_TN=cv.resize(img, None, fx=Scaling, fy=Scaling, 
+                     interpolation=cv.INTER_AREA)
+    cv.imshow(Window_Name, img_TN)
+    if(isInMain):
+        if cv.waitKey(0)==27:
+            return
+
+
 
 for i in range(start_page, page_num):
     cur_name=file_name+"_"+str(i)+".png"
@@ -106,70 +135,57 @@ for i in range(start_page, page_num):
     if(Verbose):
         print("Current IMG:", str(i))
 
-    cv.namedWindow("Press Esc for next img")
-    cv.setMouseCallback("Press Esc for next img", 
-                         readMouseInput)
+    cv.namedWindow(Window_Name)
+    cv.setMouseCallback(Window_Name, readMouseInput)
 
+    # Obtain the cutoff position for upper and lower part
     min_pos=np.argmin(np.sum(255-img_original[(4*H//10):(6*H//10), :], axis=1))
     # Upper Part
     img=img_original[0:((4*H//10)+min_pos), :]
-    img_before_erase=cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_LINEAR)
-    img_before_erase=cv.threshold(img_before_erase, 127, 255, cv.THRESH_BINARY)[1]
 
-    img=img_before_erase.copy()
     prev_img=img.copy()
     prev_img_2=img.copy()
 
-    while True:
-        cv.imshow("Press Esc for next img", img)
-        if cv.waitKey(10) == 27:
-            break
+    #while True:
+    #    img_TN=cv.resize(img, None, fx=Scaling, fy=Scaling,
+    #                      interpolation=cv.INTER_AREA)
+    #    cv.imshow(Window_Name, img_TN)
+    #    if cv.waitKey(10) == 27:
+    #        break
+    showImageAndResize(img, True)
     cv.destroyAllWindows()
 
-    img_erase_part_1=cv.bitwise_xor(img_before_erase, img)
+    print("Processing Lower")
+    img_upper=img.copy()
     
     # Lower Part
-    cv.namedWindow("Press Esc for next img")
-    cv.setMouseCallback("Press Esc for next img", 
+    cv.namedWindow(Window_Name)
+    cv.setMouseCallback(Window_Name, 
                          readMouseInput)
     img=img_original[((4*H//10)+min_pos+1):H-1, :]
-    img_before_erase=cv.resize(img, None, fx=0.5, fy=0.5)
-    img_before_erase=cv.threshold(img_before_erase, 127, 255, cv.THRESH_BINARY)[1]
 
-    img=img_before_erase.copy()
     prev_img=img.copy()
     prev_img_2=img.copy()
 
-    while True:
-        cv.imshow("Press Esc for next img", img)
-        if cv.waitKey(10) == 27:
-            break
+    #while True:
+    #    img_TN=cv.resize(img, None, fx=Scaling, fy=Scaling, 
+    #                     interpolation=cv.INTER_AREA)
+    #    cv.imshow(Window_Name, img_TN)
+    #    if cv.waitKey(10) == 27:
+    #        break
+    showImageAndResize(img, True)
     cv.destroyAllWindows()
 
-    img_erase_part_2=cv.bitwise_xor(img_before_erase, img)
-
-    # Combine upper and lower part.
-    # Then apply the erased part to original img
-    img_erase=np.vstack((img_erase_part_1, img_erase_part_2))
-    img_erase=cv.resize(img_erase, (W, H))
-
-    # Adding a Threshold can reduce speckle on the final image
-    img_erase=cv.threshold(img_erase, 80, 255, cv.THRESH_BINARY)[1]
-    img=cv.bitwise_or(img_original, img_erase)
-
     img=cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1]
-    # Despeckle again because scaling will not cover all pixels
-    (img, W_Count,B_Count)=DespecklePatch(img, 5, 10)
-    if(Verbose):
-        print("\tBlack/White Patches:", B_Count,W_Count)
 
-
-    #All done. Save file
+    # Combine upper and lower files
+    img=np.vstack((img_upper, img))
+    # All done. Save file
     save_path=final_path+file_name+"_"+str(i)+".png"
     cv.imwrite(save_path, img.astype(int),
                [cv.IMWRITE_PNG_BILEVEL, 1, cv.IMWRITE_PNG_COMPRESSION, 6])
 
-    #Change DPI. OpenCV doens't have this function, so use PIL
+    # Change DPI. OpenCV doens't have this function, so use PIL
     pimg=ImagePIL.open(save_path)
     pimg.save(save_path, dpi=(600.0, 600.0))
 end_time=time.time()
